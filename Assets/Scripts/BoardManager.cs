458d675e52359d67d89b410d3ca448cea09cda60
Aaron Ching <aaron42051@gmail.com>
2019-06-20 01:17:58 -0700

    test basic shogi board and pieces

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -2,21 +2,143 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof (MeshFilter))]
-[RequireComponent(typeof(MeshRenderer))]
-
 public class BoardManager : MonoBehaviour {
 
+    // a single spot on the board
+    public GameObject cube;
+
+    // how many cubes long the side of the board is
+    private int BOARD_SIZE = 7;
+
+    // how long a cube is
+    private float CUBE_SIZE = 1.0f;
+    private float CUBE_OFFSET = 0.5f;
+
+    // holds the shogi piece GameObjects in this order: Pawn, Lance, Knight, Silver General, Gold General, Bishop, Rook, King
+    // use it to generate pieces
+    public List<GameObject> piecePrefabs;
+    private Dictionary<string, GameObject> nameToPiece;
+
+    // list of all active pieces
+    private List<GameObject> activePieces;
+
+    // 3d array of shogi pieces, representing the board
+    public ShogiPiece[,,] ShogiPieces { set; get; }
+
+    // use to face pieces the other way
+    private Quaternion flipDirection = Quaternion.Euler(0, 180, 0);
+
+
     private void Start()
+    {
+        assignNames();
+        createBoard();
+        generateStartingPieces();
+    }
+
+    private void assignNames()
+    {
+        nameToPiece = new Dictionary<string, GameObject>();
+        nameToPiece.Add("Pawn", piecePrefabs[0]);
+        nameToPiece.Add("Lance", piecePrefabs[1]);
+        nameToPiece.Add("Knight", piecePrefabs[2]);
+        nameToPiece.Add("Silver General", piecePrefabs[3]);
+        nameToPiece.Add("Gold General", piecePrefabs[4]);
+        nameToPiece.Add("Bishop", piecePrefabs[5]);
+        nameToPiece.Add("Rook", piecePrefabs[6]);
+        nameToPiece.Add("King", piecePrefabs[7]);
+    }
+
+    // instantiates each cube that makes up the board
+    private void createBoard()
+    {
+        for (int i = 0; i < BOARD_SIZE; i++)
+        {
+            for (int j = 0; j < BOARD_SIZE; j++)
+            {
+                for (int k = 0; k < BOARD_SIZE; k++)
+                {
+                    GameObject c = Instantiate(cube, new Vector3(i, j, k), Quaternion.identity);
+                    c.transform.SetParent(transform);
+                }
+            }
+        }
+    }
+
+    private void generateStartingPieces()
+    {
+        activePieces = new List<GameObject>();
+        ShogiPieces = new ShogiPiece[BOARD_SIZE, BOARD_SIZE, BOARD_SIZE];
+
+        // generate line of Pawns in 3rd row
+        for (int i  = 0; i < BOARD_SIZE; i++)
+        {
+            // piece name, x, y, z, direction
+            SpawnPiece("Pawn", i, 0, 2, Quaternion.identity);
+        }
+
+        // generate Lances at ends of 1st row
+        SpawnPiece("Lance", 0, 0, 0, Quaternion.identity);
+        SpawnPiece("Lance", BOARD_SIZE - 1, 0, 0, Quaternion.identity);
+
+        // generate Knights next to first row Lances
+        SpawnPiece("Knight", 1, 0, 0, Quaternion.identity);
+        SpawnPiece("Knight", BOARD_SIZE - 2, 0, 0, Quaternion.identity);
+
+        // generate Silver Generals in 2nd row due to smaller board
+        SpawnPiece("Silver General", 2, 0, 1, Quaternion.identity);
+        SpawnPiece("Silver General", BOARD_SIZE - 3, 0, 1, Quaternion.identity);
+
+        // generate Golden Generals in 1st row next to center
+        SpawnPiece("Gold General", 2, 0, 0, Quaternion.identity);
+        SpawnPiece("Gold General", BOARD_SIZE - 3, 0, 0, Quaternion.identity);
+
+        // generate the King in 1st row center
+        SpawnPiece("King", 3, 0, 0, Quaternion.identity);
+    }
+
+    // spawn a shogi piece on the board
+    // provide index of piece prefab, and xyz coordinate location
+    private void SpawnPiece(string name, int x, int y, int z, Quaternion direction)
+    {
+        GameObject piece = Instantiate(nameToPiece[name], GetCubeCenter(x, y, z), direction) as GameObject;
+
+        // give the piece a transform and add it to the board
+        piece.transform.SetParent(transform);
+        ShogiPieces[x, y, z] = piece.GetComponent<ShogiPiece>();
+        
+        activePieces.Add(piece);
+    }
+
+    private Vector3 GetCubeCenter(int x, int y, int z)
+    {
+        Vector3 origin = Vector3.zero;
+        origin.x += CUBE_SIZE * x;
+        origin.y += CUBE_SIZE * y;
+        origin.z += CUBE_SIZE * z;
+
+        return origin;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    // generate a cube from code ----------------  NOT BEING USED  ---------------------
+    private void generateCube()
     {
         // creating a cube at 0, 0, 0
         Vector3[] vertices = getVertices(new Vector3(0, 0, 0));
         int[] triangles = getTriangles();
 
         createMesh(vertices, triangles);
-
-        Debug.Log(SystemInfo.graphicsShaderLevel);
-
     }
 
     // param: Vector3 of the bottom left back corner
