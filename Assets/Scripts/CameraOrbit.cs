cf7cdb661583cca0e6612595b8c8fe86a649bb93
Elton Dan <roxas190@gmail.com>
2019-06-23 04:13:30 -0700

    Camera Update
    added a test board for myself to work with and visualize things and added camera movement. needs to be finetuned tho.

---
+++ Assets/Scripts/CameraOrbit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour {
+
+    public float turnSpeed = 4.0f;
+    public Transform board;
+
+    private Vector3 offset;
+
+    void Start() {
+        offset = new Vector3(board.position.x +27.0f, board.position.y + 45.0f, board.position.z + 27.0f);
+    }
+
+    void LateUpdate() {
+        if (Input.GetMouseButton(0)) {
+            offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+            transform.position = board.position + offset;
+            transform.LookAt(board.position);
+
+            offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.left) * offset;
+            transform.position = board.position + offset;
+            transform.LookAt(board.position);
+        }
+    }
+}
